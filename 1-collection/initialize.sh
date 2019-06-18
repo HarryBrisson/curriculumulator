a8303226b47bdff9aead907fc080bce0e8d230a7
HarryBrisson <ejbrisson@gmail.com>
2019-06-18 06:16:00 -0600

    add initialize function to setup chromedriver

---
+++ 1-collection/initialize.sh
@@ -0,0 +1,21 @@
+
+sudo apt-get update
+
+# install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+# install chrome driver
+wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+
+# set permissions for chrome driver
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
+sudo apt-get -y install python3-pip
+sudo python3 -m pip install -r requirements.txt
