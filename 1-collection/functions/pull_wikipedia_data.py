48df3d60803ecd8a1a105344cb596c7d75b9ac09
HarryBrisson <ejbrisson@gmail.com>
2019-06-18 20:40:31 -0600

    add function to get articles data for search query

--- 1-collection/functions/pull_wikipedia_data.py
+++ 1-collection/functions/pull_wikipedia_data.py
@@ -7,3 +7,25 @@ def get_monthly_data_for_topic(topic):
   url = f'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/{article}/monthly/2016010100/2019123100'
   r = requests.get(url)
   return r.json()
+
+
+def get_articles_data_from_search_query(q):
+  
+  URL = "https://en.wikipedia.org/w/api.php"
+
+  SEARCHPAGE = "biology"
+
+  PARAMS = {
+      'action':"query",
+      'list':"search",
+      'srsearch': q,
+      'format':"json",
+      'prop':'info',
+      'inprop':'url'
+  }
+
+  r = requests.get(url=URL, params=PARAMS)
+  search_data = r.json()['query']
+
+  return search_data
+
