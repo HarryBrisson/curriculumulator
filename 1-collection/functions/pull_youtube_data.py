eb8584e8710093fd4359a1c7394c3aa8d894ad2b
HarryBrisson <ejbrisson@gmail.com>
2019-06-21 22:56:48 -0600

    add function get video data for a search term

--- 1-collection/functions/pull_youtube_data.py
+++ 1-collection/functions/pull_youtube_data.py
@@ -8,3 +8,24 @@ def get_youtube_api_key():
 		api_key=f.read().replace("\n","")
 	return api_key
 
+def get_videos_for_search_term(q):
+
+	api_key = get_youtube_api_key()
+
+	params = {
+		'part':'snippet',
+		'order':'viewCount',
+		'q':q,
+		'type':'video',
+		'key':api_key,
+	}
+
+	uri = 'https://www.googleapis.com/youtube/v3/search'
+
+
+	print("searching for vids relating to "+q)
+	r = requests.get(uri,params=params)
+
+	return r.json()
+
+
