1fc84b3ad403c36488100fdd049a4f78f8755aea
HarryBrisson <ejbrisson@gmail.com>
2019-06-21 22:56:30 -0600

    add function to pull api key from auth folder

--- 1-collection/functions/pull_youtube_data.py
+++ 1-collection/functions/pull_youtube_data.py
@@ -3,3 +3,8 @@ import time
 
 import pandas as pd
 import requests
+def get_youtube_api_key():
+	with open(r"authorizations/youtube-api-key.txt", 'r') as f:
+		api_key=f.read().replace("\n","")
+	return api_key
+
