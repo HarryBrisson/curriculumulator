1396c18dedf8c850195399f515c741d06597a68d
HarryBrisson <ejbrisson@gmail.com>
2019-06-21 22:57:05 -0600

    add function to pull stats for a youtube video

--- 1-collection/functions/pull_youtube_data.py
+++ 1-collection/functions/pull_youtube_data.py
@@ -29,3 +29,51 @@ def get_videos_for_search_term(q):
 	return r.json()
 
 
+def get_video_stats(video_id):
+
+	stats = {}
+
+	api_key = get_youtube_api_key()
+	url = r"https://www.googleapis.com/youtube/v3/videos?part=statistics&id="+video_id+"&key="+api_key
+	print("pulling stats for "+video_id)
+
+	for trial in range(5):
+		try:
+			r = requests.get(url, timeout=5)
+			d = json.loads(r.text)['items'][0]
+			break
+		except Exception as e:
+			print(e)
+			if trial < 2:
+				print("trying again")
+			else:
+				print("moving on")
+				d = {}
+
+	try:
+		stats['comments'] = d['statistics']['commentCount']
+	except:
+		stats['comments'] = ''
+
+	try:
+		stats['likes'] = d['statistics']['likeCount']
+	except:
+		stats['likes'] = ''
+
+	try:
+		stats['dislikes'] = d['statistics']['dislikeCount']
+	except:
+		stats['dislikes'] = ''
+
+	try:
+		stats['views'] = d['statistics']['viewCount']
+	except:
+		stats['views'] = ''
+
+	# ideally want this to match the youtube datetime format for easy comparison
+	stats['collection_datetime'] = datetime.datetime.utcnow()
+
+
+	return stats
+
+
