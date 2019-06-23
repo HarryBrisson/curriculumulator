2e370d1154a575901f7b5caa7eb192cfb3b8e006
HarryBrisson <ejbrisson@gmail.com>
2019-06-22 23:07:17 -0600

    add function to pull scorecard data

--- 3-presentation/functions/curriculumulator.py
+++ 3-presentation/functions/curriculumulator.py
@@ -12,3 +12,35 @@ def get_curriculumulator_data():
 		}
 
 	return data
+
+
+def get_scorecard_data(subject):
+    
+    df = pd.read_csv('static/data/summary.csv')
+
+    pctls = pd.DataFrame()
+    indices = pd.DataFrame()
+
+    for c in df.columns:
+        try:
+            pctls[c] = df[c].rank(pct=True)
+            indices[c] = df[c]/(df[c].mean())*100
+        except:
+            pctls[c] = df[c]
+            indices[c] = df[c]
+
+    pctls = pctls[pctls['dept']==subject].transpose()
+    indices = indices[indices['dept']==subject].transpose()
+    df = df[df['dept']==subject].transpose()
+
+
+    pctls = pctls.rename(columns={df.columns[0]:'percentile'})
+    indices = indices.rename(columns={df.columns[0]:'index'})
+    df = df.rename(columns={df.columns[0]:'score'})
+
+
+    df = df.join(pctls)
+    df = df.join(indices)
+
+    return df
+
