e2f7599b3cc32b35b09f80de2f7626839a855c35
HarryBrisson <ejbrisson@gmail.com>
2019-08-09 23:41:40 -0400

    add function to get reddit client secret

--- 1-collection/functions/pull_reddit_data.py
+++ 1-collection/functions/pull_reddit_data.py
@@ -1,7 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import praw
 
+
+def get_client_secret():
+    with open(r"authorizations/reddit-secret.txt", 'r') as f:
+        api_key=f.read().replace("\n","")
+    return api_key
 
 
 def main():
