ac33123d453b76bdb6e29c774e3eaa5b23d0d729
HarryBrisson <ejbrisson@gmail.com>
2019-08-09 23:41:54 -0400

    start function to pull homeworkhelp posts

--- 1-collection/functions/pull_reddit_data.py
+++ 1-collection/functions/pull_reddit_data.py
@@ -9,6 +9,11 @@ def get_client_secret():
         api_key=f.read().replace("\n","")
     return api_key
 
+def identify_homeworkhelp_posts():
+    reddit = praw.Reddit(client_id='am_DaL0ACH9R7Q',
+                     client_secret=get_client_secret(),
+                     user_agent='ubuntu:curriculumular:v0.0.1 (by /u/NotSorryImSorry)')
+    print(reddit)
 
 def main():
     return
