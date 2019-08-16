73f3e2b7771b13ff7bed154b17d1b736d6889bd1
HarryBrisson <ejbrisson@gmail.com>
2019-08-15 21:56:41 -0500

    add function to pull subreddit submissions for a given date

--- 1-collection/functions/pull_reddit_data.py
+++ 1-collection/functions/pull_reddit_data.py
@@ -32,6 +32,17 @@ def identify_homeworkhelp_posts():
       
     return data
 
+def get_subreddit_posts_for_date(subreddit,date):
+    utc_time = time.strptime(date, "%Y-%m-%d")
+    d1 = timegm(utc_time)
+    d2 = d1 + (24 * 60 * 60)
+
+    url = f"https://api.pushshift.io/reddit/search/submission/?subreddit=homeworkhelp&size=500&after={d1}&before={d2}"
+    print(url)
+    r = requests.get(url)
+    data = r.json()['data']
+    return data
+
 def main():
     data = identify_homeworkhelp_posts()
     df = pd.DataFrame(data)
