9ffd834600eee8f96c96fa5feb80070a2e9695fe
HarryBrisson <ejbrisson@gmail.com>
2019-08-20 18:23:49 -0400

    add function to get subjects from presentation csv

--- 2-analysis/aggregate_reddit_data.py
+++ 2-analysis/aggregate_reddit_data.py
@@ -15,6 +15,10 @@ def create_aggregated_reddit_json_file():
 	df = import_all_reddit_jsons()
 	df.to_json('data/reddit.json',orient='records')
 
+def get_subjects():
+	subjects = list(pd.read_csv('../3-presentation/static/data/summary.csv')['dept'])
+	return subjects
+
 def calculate_subject_scores(subjects):
 	df = pd.read_json('data/reddit.json')
 	for s in subjects:
